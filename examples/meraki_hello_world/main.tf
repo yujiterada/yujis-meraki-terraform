577eea42b15ec06f0865046c577e2e12a3c7e3a3
Yuji Terada <yuji.terada.jp@gmail.com>
2024-11-14 00:20:50 +1100

    add comment to change network name

--- examples/meraki_hello_world/main.tf
+++ examples/meraki_hello_world/main.tf
@@ -9,12 +9,13 @@ terraform {
 
 provider "meraki" {
   # Change the below to your Meraki API key.
-  meraki_dashboard_api_key = "12345"
+  meraki_dashboard_api_key = "a769b73ffc7df5ed9eb6f0a688b824cc30032427"
 }
 
 resource "meraki_networks" "test_network" {
+  # Change the below to the name of the network you want to create.
   name            = "Test network"
   # Change the below to your Meraki organization ID. This can be found at the bottom of Meraki dashboard.
-  organization_id = "12345"
+  organization_id = "892608"
   product_types   = ["appliance", "switch", "wireless"]
 }
